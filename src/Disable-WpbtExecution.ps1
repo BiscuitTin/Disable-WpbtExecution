471e9a2f3cb7104e61a9768482acb110188611e3
Kexy Biscuit <kexybiscuit@biscuitt.in>
2020-05-22 18:57:28 +0800

    Add errors when the script...
      - Isn't running on Windows.
      - Is running without elevated permissions.

--- src/Disable-WpbtExecution.ps1
+++ src/Disable-WpbtExecution.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.2
+.VERSION 1.0.3
 
 .GUID a73568ed-d3da-4b81-9522-d29b69b97010
 
@@ -25,7 +25,7 @@
 .EXTERNALSCRIPTDEPENDENCIES
 
 .RELEASENOTES
-Make use of positional parameters.
+Add errors when the script isn't running on Windows or is running without elevated permissions.
 
 .PRIVATEDATA
 
@@ -38,12 +38,24 @@ Make use of positional parameters.
 
 #> 
 Param(
-    [Parameter(Mandatory=$true,Position=0,
-    HelpMessage="Specify the .wim file to mount")]
-    [string] $ImageFile
+  [Parameter(Mandatory=$true,Position=0,
+  HelpMessage="Specify the .wim file to mount")]
+  [string] $ImageFile
 )
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
+
+if (-not (($null -eq $PSEdition) -or ($PSEdition -eq "Desktop") -or ($PSVersionTable.Platform -eq "Win32NT")))
+{
+  Write-Error "Microsoft Windows is required to run Disable-WpbtExecution."
+  break
+}
+
+if (-not ([System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+  Write-Error "Elevated permissions are required to run Disable-WpbtExecution."
+  break
+}
 
 Dism.exe /Get-ImageInfo /ImageFile:"$ImageFile"
 [int] $index = Read-Host "Specify the index of the image on which to perform the disable operation"
